cc03391a20ae10395a86cae5161312bef93544c5
Tommy Zng <1319524930@qq.com>
2015-09-13 21:44:34 +0800

    Update bdtz.h

--- bdtz.h
+++ bdtz.h
@@ -13,6 +13,7 @@
 #define fdxe(i,f,t) for(int i=f;i>=t;--i)
 #define pi 3.14159265358979323846264
 #define ifnext(a,b) (((a)-1)&b)
+#define LibraryLoaded_bdtz
 typedef long long ll;
 typedef unsigned long long ull;
 typedef int il;
